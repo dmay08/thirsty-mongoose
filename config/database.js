0fb1843c76628325b998a5954f72dac532dbfc39
danny mayasich <dannymayasich@dannys-MacBook-Pro.local>
2018-10-26 14:26:03 -0700

    first commit

---
+++ config/database.js
@@ -0,0 +1,10 @@
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/thirsty-mongoose',
+    {useNewUrlParser: true}
+);
+
+var db = mongoose.connection;
+db.once('open', function() {
+    console.log('Connected to MongoDB at')
+})
